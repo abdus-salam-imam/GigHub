dabc20d401b5e70de158c0a2a9d6c3dff96df605
abdus salam <abdul1salam@hotmail.com>
2020-05-26 02:21:07 +0500

    Create Add a Gig Form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,45 @@
+﻿using GigHub.Models;
+using GigHub.viewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        
+        public ActionResult New()
+        {
+
+
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new GigsFormViewModel
+            {
+                Genres=genres
+
+
+            };
+
+
+            return View(viewModel);
+        }
+
+
+
+
+    }
+}
---
+++ GigHub/ViewModels/GigsFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.viewModels
+{
+    public class GigsFormViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Venue { get; set; }
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public Genre GenreId { get; set; }
+
+    }
+}
